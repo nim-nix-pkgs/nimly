ccd445743cd48ce324bdf53ab5d915a5a026453d
hugosenari <hugosenari@gmail.com>
2022-03-12 04:29:31 -0300

    chore: re index nimly

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Lexer Generator and Parser Generator as a Macro Library in Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimly-master.flake = false;
+  inputs.src-nimly-master.owner = "loloicci";
+  inputs.src-nimly-master.ref   = "refs/heads/master";
+  inputs.src-nimly-master.repo  = "nimly";
+  inputs.src-nimly-master.type  = "github";
+  
+  inputs."patty".dir   = "nimpkgs/p/patty";
+  inputs."patty".owner = "riinr";
+  inputs."patty".ref   = "flake-pinning";
+  inputs."patty".repo  = "flake-nimble";
+  inputs."patty".type  = "github";
+  inputs."patty".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."patty".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimly-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimly-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
